1243f6d77aaa5d63999653550b30b47a28e1a75c
Jyoti-prakash-Nayak <87771215+Jyoti-prakash-Nayak@users.noreply.github.com>
2022-11-06 11:09:39 +0530

    This is a chart app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import {Route} from "react-router-dom";
+import Homepage from './Pages/HomePage';
+import ChatPage from './Pages/ChatPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Route path="/" exact component={Homepage}/>
+      <Route path="/chats"  component={ChatPage}/>
+    </div>
+  );
+}
+
+export default App;
